38313ccb52c3ead5b16f926ee7387aac45587c18
root <mudakizevictor@gmail.com>
2022-06-28 05:39:46 -0700

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,4 +12,4 @@ elif last < 6 and last != 0:
     stmt = "and is less than 6 and not 0"
 else:
     stmt = "and is 0"
-print(f"Last digit of {number} is {last} and {stmt}")
+print(f"Last digit of {number} is {last} {stmt}")
