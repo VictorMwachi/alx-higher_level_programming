81056fcabd8a5584b8eaea97d74735d727e5861d
root <mudakizevictor@gmail.com>
2022-06-28 06:18:12 -0700

    üpdated

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-  if i != 99:
-    print("{:2d}",end = ",")
-   else:
-    print("{i})
+for i in range(0, 100):
+    if i != 99:
+        print(f"{i:02d}",end = ",")
+    else:
+        print(f"{i:d}")
