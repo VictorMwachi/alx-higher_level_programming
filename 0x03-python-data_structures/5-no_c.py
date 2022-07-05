8eefb70159617d523adfe8e0a6b86b5491d1b5aa
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-05 22:55:47 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-   return my_string.translate({ord(c):None for c in "cC"})
+   return my_string.translate({ord(c):
+                               None for c in "cC"})
