a233d7f8e73bce6bc74e4a68991c74b58fad99d3
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-05 15:46:53 +0300

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    my_list2 = my_list.copy()
+    my_list2 = my_list[:]
     if idx < 0 or idx >= len(my_list):
         return my_list2
     else:
