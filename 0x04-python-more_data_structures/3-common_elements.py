e6a10ba3e159af8668623f3fbc45d4ddb664c391
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-06 16:10:04 +0300

    Update 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,6 +2,8 @@
 def common_elements(set_1, set_2):
     common = []
     for x in set_1:
-        if x is in set_2:
+        if x not in set_2:
+            continue
+        else:
             common.append(x)
     return common
