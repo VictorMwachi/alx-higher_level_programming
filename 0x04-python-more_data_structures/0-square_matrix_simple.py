609354a33d753caa978ca9f552b3b62f56b65d0e
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-06 12:58:57 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix = []):
+def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
         s_row = []
         for i in row:
-            s_row.append(i ** 2)
+            s_row.append(i**2)
         new_matrix.append(s_row)
     return new_matrix
