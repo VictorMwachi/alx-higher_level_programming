cbe87d99909fcf1073105e2799fd9f918cc23369
root <mudakizevictor@gmail.com>
2022-07-06 05:29:21 -0700

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-  for s in range(len(my_list)-1):
-    if my_list[s] == search:
-    my_list[s] = replace
-  return my_list
+    for s in range(len(my_list)-1):
+        if my_list[s] == search:
+            my_list[s] = replace
+        return my_list
