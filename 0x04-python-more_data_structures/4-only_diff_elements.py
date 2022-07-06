cb3536691cf42a94e1ac8470a0d1d7fd4e373595
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-06 16:15:29 +0300

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = []
+    diff = []
     for x in set_1:
         if x not in set_2:
-            continue
-        else:
-            common.append(x)
-    return common
+            diff.append(x)
+    return diff
