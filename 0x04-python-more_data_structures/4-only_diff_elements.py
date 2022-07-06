bbcd860f5ddcd484ad2eabca7ca9244c82b123d1
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-06 16:25:24 +0300

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,4 +4,7 @@ def only_diff_elements(set_1, set_2):
     for x in set_1:
         if x not in set_2:
             diff.append(x)
+    for x in set_2:
+        if x not in set_1:
+            diff.append(x)
     return diff
