581c653eea84d765882ad9e3a46ff315a951a41d
root <mudakizevictor@gmail.com>
2022-07-12 05:07:13 -0700

    1-search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     my_new_list = []
-    for s in range(len(my_list)-1):
+    for s in range(len(my_list)):
         if my_list[s] == search:
             my_new_list.append(replace)
         else:
