d23c24233fc74930ce2c618028558bc4d55d38d1
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-12 15:00:54 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    my_new_list = []
     for s in range(len(my_list)-1):
         if my_list[s] == search:
-            my_list[s] = replace
-            continue
-    return my_list
+            my_new_list.append(replace)
+    return my_new_list
