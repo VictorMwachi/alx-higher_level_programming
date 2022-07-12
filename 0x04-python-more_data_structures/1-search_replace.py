e46cd2acf8179c7308c7f402ea5ac45f1cfb8fe7
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-12 14:52:11 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,4 +3,5 @@ def search_replace(my_list, search, replace):
     for s in range(len(my_list)-1):
         if my_list[s] == search:
             my_list[s] = replace
+            continue
     return my_list
