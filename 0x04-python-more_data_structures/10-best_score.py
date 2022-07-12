b4f17b98cebaef437172f5bc5098ba792d063e50
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-12 15:27:15 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-  keys_list = []
-  for key in a_dictionary.keys:
-    keys_list.append(key)
-  return(max(keys_list))
+  return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
