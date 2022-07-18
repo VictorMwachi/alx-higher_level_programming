e5e50f948dcf591e80dfaff767577829e614a808
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-18 14:47:24 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,5 +4,4 @@ def safe_print_list(my_list = [], x = 0):
         for i in range(x):
             print("{}".format(my_list[i]),end="")
     except IOError:
-        print("error")
-print()
+        print()
