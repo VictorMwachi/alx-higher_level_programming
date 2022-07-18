a2e64fc6c24289efc7b2ea6dde3f6c9a4d3a459b
root <mudakizevictor@gmail.com>
2022-07-18 06:11:15 -0700

    excep

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    try:
+        for n in range x:
+            if isinstance(my_list[n], int):
+                print(my_list[n])
+    except:
