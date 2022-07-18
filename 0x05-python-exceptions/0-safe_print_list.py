5939b43c234efa476beb6d72ec2cc7c08dd20476
root <mudakizevictor@gmail.com>
2022-07-18 05:03:23 -0700

    pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
             if i < x:
                 print("{}".format(my_list[i]), end="")
             else:
-                print()          
+                print()
     except IndexError:
         print()
