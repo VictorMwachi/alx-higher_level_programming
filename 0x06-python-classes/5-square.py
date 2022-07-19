269eaf5d571a57602f766982b921a62da0d0bbac
root <mudakizevictor@gmail.com>
2022-07-19 14:34:06 -0700

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""class square"""
+
+
+class Square:
+    """initialise class square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """function that returns area of square"""
+
+    def area(self):
+        return self.__size ** 2
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(size):
+                for j in range(size):
+                    print('#',end='')
