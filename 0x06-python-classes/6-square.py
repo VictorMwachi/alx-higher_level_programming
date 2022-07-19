88e99ac6823b97dc13ae8901622bf74c63a5335f
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-20 01:36:10 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,12 +5,17 @@
 class Square:
     """initialise class square"""
 
-    def __init__(self, size=0):
-        self.__size = size
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
         return self.__size
+    
+    @property
+    def position(self):
+        return self.__position
 
     @size.setter
     def size(self, value):
@@ -19,6 +24,11 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+        
+        @position.setter
+        def position(self, value):
+            if type(value) is not tuple or len(value) != 2 or type(value[0]) is not int or type(value[1]) is not int or type(value[0]) < 0 or type(value[1]) < 0:
+                raise TypeError("position must be a tuple of 2 positive integers")
 
     """function that returns area of square"""
 
