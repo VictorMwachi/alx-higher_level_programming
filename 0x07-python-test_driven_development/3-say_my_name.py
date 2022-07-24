bcc083cf8731331d9a338351626f859a990c7b76
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-24 14:30:47 +0300

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+  """returns concat of first and last name with space in between"""
+  
+  if type(first_name) is not string:
+    raise TypeError("first_name must be a string")
+  if type(last_name) is not string:
+    raise TypeError("last_name must be a string"
+  return "My name is "first name + " " + last name
