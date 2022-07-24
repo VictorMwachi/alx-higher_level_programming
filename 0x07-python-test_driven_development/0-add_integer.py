b6b0214938ec1a655f1cde82a247b7548953d07d
root <mudakizevictor@gmail.com>
2022-07-24 02:18:28 -0700

    0-add_integeere.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def add_integer(a, b=98):
+    """adds integers"""
+    if a is float:
+        a = int(a)
+    if b is float:
+        b = int(b)
+    if a is not int:
+        raise TypeError("a must be an integer")
+    if b is not int:
+        raise TypeError("b must be an integer")
+    return a + b
