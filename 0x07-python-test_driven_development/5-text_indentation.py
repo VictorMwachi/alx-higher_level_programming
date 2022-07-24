36916fe7d2f75fd3f7120b9ac3b28dfdd4085517
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-24 20:11:15 +0300

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 def text_indentation(text):
-  cha=[':','.','?']
-  if type(text) is not str:
-    raise TypeError("text must be a string")
-    for ch in text:
-      if ch in cha:
-        print()
-      else:
-        print(ch,end="") 
+    after = None
+    cha=[':','.','?']
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for i in range(0,len(text)):
+        if text[i] in cha:
+            print(text[i]+"\n")
+            after = True
+        else:
+            if after is True:
+                if text[i] == ' ':
+                    if text[i+1] != ' ':
+                        after = False
+                else:
+                    print(text[i],end="") 
