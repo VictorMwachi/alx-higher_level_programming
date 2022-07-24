88754ca6ce081ca5be526cf18373bcee51ed4da5
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-24 15:04:00 +0300

    Create 4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_square(size):
+  """returns a square print in #"""
+  if type(size) != int and type(size) is float and size < 0:
+    raise TypeError("size must be an integer")
+  if size < 0:
+    raise ValueError("size must be >= 0")
+  for r in range(0,size):
+    print('#' * size)
