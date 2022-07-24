cc23d657715936683fa3ae27382a7971d73689ba
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-24 13:21:20 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
     """divides evey element in a matrix"""
+    new_matrix = []
     for row in matrix:
         for i in row:
             if type(i) != float or type(i) != int:
@@ -8,4 +9,13 @@ def matrix_divided(matrix, div):
     for row in matrix:
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
-
+    if type(div) != int or type(div) != float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for row in matrix:
+        new_row = []
+        for i in row:
+            new_row.append(i/div)
+        new_matrix.append(new_row)
+    return new_matrix    
