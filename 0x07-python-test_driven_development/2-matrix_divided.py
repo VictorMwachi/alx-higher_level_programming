62f5874d1cf4221f2e9b9fe5d69a6783a0a05b23
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-24 12:52:28 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
   """divides evey element in a matrix"""
+  for row in matrix:
+    for i in row:
+      if type(i) != float or type(i) != int:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")     
