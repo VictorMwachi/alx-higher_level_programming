2771bf95e45ce30e7529eb80f5251f60bc2ef969
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-27 14:18:07 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Rerctangle module"""
+"""Rectangle module"""
 
 
 class Rectangle:
-    """class rectangle
-    Initialize Rectangle object with height and width"""
+    """Initialize Rectangle object with height and width"""
 
     def __init__(self, width=0, height=0):
         self.__width = width
