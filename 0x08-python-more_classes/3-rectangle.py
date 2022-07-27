6649cfffeeae46c88c902b3b447b7e34758d08e6
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-27 16:37:02 +0300

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -64,6 +64,6 @@ class Rectangle:
         """
         rep_str = ""
         if self.__width != 0 and self.__height != 0:
-            rep_str += ("\n".join("#" * self.__width 
-                for i in range(self.__height)))
+            rep_str += ("\n".join("#" * self.__width
+                                   for i in range(self.__height)))
         return rep_str
