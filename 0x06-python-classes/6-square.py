d5df74ca1cb121697d9282d769994d7f8e2bddbe
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-07-28 14:23:20 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -21,5 +21,27 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self):
-        return (self.size * self.size)
+        @position.setter
+    def position(self, value):
+        """"set position"""
+        if (type(value) is not tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (len(value) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (type(value[0]) is not int) or (type(value[1]) is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (value[0] < 0) or (value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+            
+    def my_print(self):
+        """print the square"""
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.position[1]):
+                print("")
+            for i in range(self.size):
+                print(" " * self.position[0], end="")
+                print("#" * self.size)
