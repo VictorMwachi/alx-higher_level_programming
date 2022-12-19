f95f3f1d8728b6fc327949e103ca6648e30de713
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-12-19 09:55:30 +0300

    102-magic_calculation.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            if i < x:
-                print("{}".format(my_list[i]), end="")
-            else:
-                print()
-    except IndexError:
-        print()
+    counter = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError:
+            break
+        else:
+            counter += 1
+    print()
+    return counter
