34835969279eccde9f74537c606954d487c542cb
victor mwachi <mudakizevictor@gmail.com>
2022-12-23 00:58:05 -0800

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+checks if  is a subclass of a specified class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns true or false wheteher an object
+    is a subclass of specified class"""
+    return type(obj) == a_class
