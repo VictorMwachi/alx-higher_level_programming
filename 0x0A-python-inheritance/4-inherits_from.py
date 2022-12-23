a0a504aafa735a7689ceff8629262653d1265b35
victor mwachi <mudakizevictor@gmail.com>
2022-12-23 01:22:56 -0800

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+checks if  is a subclass of a specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """returns true or false wheteher an object
+    is a subclass of specified class"""
+    return isinstance(obj, a_class)
