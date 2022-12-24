34952f8cdb3ed84c31703e4c7b588be0493ef1a5
victor mwachi <mudakizevictor@gmail.com>
2022-12-24 11:29:41 -0800

    8-rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,14 +8,14 @@ class BaseGeometry:
     def area(self):
         """returns an exception"""
         raise Exception("area() is not implemented")
-        
+
     def integer_validator(self, name, value):
         """
          validates value:
          you can assume name is always a string
          if value is not an integer: raise a TypeError exception,
          with the message <name> must be an integer
-         if value is less or equal to 0: raise a ValueError exception 
+         if value is less or equal to 0: raise a ValueError exception
          with the message <name> must be greater than 0
          """
         if type(value) is not int:
