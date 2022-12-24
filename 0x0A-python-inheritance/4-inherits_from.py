ac3a680957d3be3b697b1fde3cb5be87ed8e4f51
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-12-24 19:54:59 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ checks if  is a subclass of a specified class
 def inherits_from(obj, a_class):
     """returns true or false wheteher an object
     is a subclass of specified class"""
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) != a_class
