189bfde4cc65d4630f8ea053cc0df83492ff939f
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-12-24 22:48:58 +0300

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,21 +5,6 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def integer_validator(self, name, value):
-        """
-         validates value:
-         you can assume name is always a string
-         if value is not an integer: raise a TypeError exception,
-         with the message <name> must be an integer
-         if value is less or equal to 0: raise a ValueError exception
-         with the message <name> must be greater than 0
-         """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
 
 class Rectangle(BaseGeometry):
     """class rectangle"""
