cb1f5788ea075684a5d152ce958fea9672e70f0c
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-12-24 22:24:35 +0300

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,8 +13,10 @@ class BaseGeometry:
         """
          validates value:
          you can assume name is always a string
-         if value is not an integer: raise a TypeError exception, with the message <name> must be an integer
-         if value is less or equal to 0: raise a ValueError exception with the message <name> must be greater than 0
+         if value is not an integer: raise a TypeError exception,
+         with the message <name> must be an integer
+         if value is less or equal to 0: raise a ValueError exception 
+         with the message <name> must be greater than 0
          """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
