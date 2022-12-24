d98dfad9e6dd119046370b5382dee796bc3b9b1a
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-12-24 22:23:37 +0300

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,6 +8,7 @@ class BaseGeometry:
     def area(self):
         """returns an exception"""
         raise Exception("area() is not implemented")
+        
     def integer_validator(self, name, value):
         """
          validates value:
