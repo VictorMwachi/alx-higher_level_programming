7c11b3ac2e8667fa12c54e544bae6f96135d4c83
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-12-26 09:51:11 +0300

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ module contains a function for reading a file
 
 def read_file(filename=""):
     """reads a file"""
-    with open(filename,encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
