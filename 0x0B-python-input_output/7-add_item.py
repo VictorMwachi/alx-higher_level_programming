a0feef7a6f9e379f9a0549b78147724e7b7a997c
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-12-26 13:03:23 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,5 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-print("enter arguments seperated by space", sys.argv[0])
 save_to_json_file(sys.argv[1:],"add_item.json")
 load_from_json_file("add_item.json")
