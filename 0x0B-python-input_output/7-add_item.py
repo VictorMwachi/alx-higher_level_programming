42e594bc8ac5b4ff63ad850cfbff50f24c02b881
victor mwachi <mudakizevictor@gmail.com>
2022-12-26 01:56:44 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,14 +5,11 @@ a Python list, and then save them to a file
 """
 
 
-import sys.argv
+import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 print("enter arguments seperated by space", sys.argv[0])
-temp=[]
-for i in range(1:len(sys.argv)):
-        temp.append(sys.argv[i])
-save_to_json("add_item.json",temp)
-load_from_json("add_item.json")
+save_to_json_file(sys.argv[1:],"add_item.json")
+load_from_json_file("add_item.json")
