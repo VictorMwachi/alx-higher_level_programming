5021e48a30e7c9655731545e04f2162444c13894
victor mwachi <mudakizevictor@gmail.com>
2022-12-26 03:52:20 -0800

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module contains class student
+"""
+
+
+class Student:
+    """defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """initialises class student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            dict_new={}
+            for att in attrs:
+                if hasattr(self,att):
+                    dict_new[att] = getattr(self, att)
+            return dict_new
