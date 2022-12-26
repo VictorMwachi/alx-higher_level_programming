718e86e5cc00c45fffb519521d8d9434c0309d78
victor mwachi <mudakizevictor@gmail.com>
2022-12-25 23:42:48 -0800

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module contains a function that
+returns JSON representation of
+an object(string)
+"""
+
+
+def to_json_string(my_obj):
+    """reurns string rep of an object"""
+    return json.dumps(my_obj)
