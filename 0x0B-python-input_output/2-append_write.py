343ae25e8416073f7598df76a28a931b326d45b0
victor mwachi <mudakizevictor@gmail.com>
2022-12-25 23:31:27 -0800

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+module contains function for appending a file
+"""
+
+
+def append_write(filename="", text=""):
+    """appends text to a file and retuns the updated file"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return(f.append(text))
