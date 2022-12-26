b74ab6b30e0ee472e16c1a9a0ab26a233c186f07
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-12-26 10:20:18 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ module contains function for writing file
 def write_file(filename="", text=""):
     """writes text in file in utf-8 formart"""
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)
