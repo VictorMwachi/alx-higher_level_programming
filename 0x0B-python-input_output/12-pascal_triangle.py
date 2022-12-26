5304f324174370ec80e38bd328d6c84820aa2053
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-12-26 16:37:46 +0300

    12-pascal_triangle.p

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,10 +1,38 @@
 #!/usr/bin/python3
-"""
-a function def pascal_triangle(n): that returns a
-list of lists of integers representing the Pascal’s triangle of n
-"""
+""" pascal_triangle module """
 
 
-def pascal_triangle(n):
-    """reurns a list of integers representing pascal triangle"""
+def pascal_triangle(prmN):
+    """
+        Function that define a Pascal triangle
+        Args:
+            prmN: size of the triangle
+    """
 
+    triangle = []
+    if prmN <= 0:
+        return triangle
+
+    for i in range(prmN):
+        row = []
+        for j in range(prmN):
+            if (j > i):
+                break
+            if i > 0 and len(triangle) == i and len(triangle[i - 1]) > j:
+                y = triangle[i - 1][j]
+            elif len(triangle) >= i:
+                y = 0
+            if (
+                i > 0 and len(triangle) > i - 1 and
+                j > 0 and len(triangle[i - 1]) > j - 1
+            ):
+                x = triangle[i - 1][j - 1]
+            else:
+                x = 0
+            if x == 0 and y == 0:
+                row.append(1)
+            else:
+                row.append(x + y)
+        triangle.append(row)
+
+    return triangle
