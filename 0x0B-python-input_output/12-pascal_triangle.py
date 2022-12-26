45fb8ad268f8e536687cbb0d38e6c856c165c22b
victor mwachi <mudakizevictor@gmail.com>
2022-12-26 05:31:18 -0800

    padscal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+a function def pascal_triangle(n): that returns a
+list of lists of integers representing the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """reurns a list of integers representing pascal triangle"""
+
