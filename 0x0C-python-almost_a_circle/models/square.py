f7ff952bcb72ac0e3bc95dcf01af2ae644fdfafd
victormwachi <mudakizevictor@gmail.com>
2022-12-27 21:36:59 +0300

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,7 +11,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """defines our square"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, size, size)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """custom string defination of square"""
