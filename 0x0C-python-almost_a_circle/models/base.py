7667a2fab0a85ba0de58e9f35487657b39accd6e
victormwachi <mudakizevictor@gmail.com>
2022-12-28 15:03:49 +0300

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,7 +4,7 @@ module has class base
 """
 
 
-import json
+import json, os
 
 
 class Base:
@@ -48,3 +48,27 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ == 'Rectangle':
+            new = cls(5, 5)
+        else:
+            new = cls(10)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        filename = f"{cls.__name__}.json"
+        if os.path.exists(filename) is False:
+            return []
+        else:
+            with open(filename, encoding='utf-8') as f:
+                json_str = f.read()
+        list_dic = cls.from_json_string(json_str)
+        list_ins = []
+        for i in range(len(list_dic)):
+            list_ins.append(cls.create(**list_dic[i]))
+        return list_ins
