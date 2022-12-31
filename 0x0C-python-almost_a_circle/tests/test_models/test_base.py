72577b454f5f799ad35a055a064ce96559f97b32
victormwachi <mudakizevictor@gmail.com>
2022-12-31 11:55:15 +0300

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -13,4 +13,7 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     def test_init(self):
         b = Base()
-        self.assertIsNotNone(b)
+        b1 = Base()
+        b2 = Base()
+        self.assertIsNotNone(b.id)
+        self.assertGreater(b.id, 0)
