7777030f2c7546d9474370f64a0ec46986493c4e
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-01-06 21:25:27 +0300

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,11 +3,13 @@ const square = require('./5-square');
 
 class Square extends square {
   charPrint (c) {
-    if (c === undefined) {
-      console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
-    } else {
-      console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
+    if (c == null) {
+      c = 'X';
     }
+    let iter = size;
+        while(iter>0){
+            console.log('X'.repeat(size));
+            iter--;
   }
 }
 module.exports = Square;
