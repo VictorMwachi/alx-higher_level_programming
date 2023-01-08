72f324265e478ef0d88c1db74768f9e146be57d0
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-01-08 23:25:50 +0300

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
         cursor = db_connection.cursor()
         state_name = sys.argv[4]
         cursor.execute(
-            'SELECT * FROM states WHERE CAST(name AS BINARY) LIKE ' +
-            'CAST("{}" AS BINARY) ORDER BY id ASC;'.format(state_name)
+            f'SELECT * FROM states WHERE CAST(name AS BINARY) LIKE ' +
+            'CAST("{state_name}" AS BINARY) ORDER BY id ASC;'
         )
         results = cursor.fetchall()
         for result in results:
