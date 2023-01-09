3f5a072bfaac5ffe3a863515c637ff85d1fc251a
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-01-09 15:53:35 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         html = response.read()
 
         print('Body response:')
-        print(f'\t- type:{type(html)}')
+        print(f'\t- type: {type(html)}')
         print(f'\t- content: {html}')
         print(f'\t- utf8 content: {html.decode("utf-8", "replace")}')
