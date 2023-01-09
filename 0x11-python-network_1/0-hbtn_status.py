545a1cbc669d4560b211288933acd6185ccd4e6c
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-01-09 15:45:09 +0300

    0-hbtn_status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-import urllib.request
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    html = response.read()
-    print("Body response:")
-    print(f"\t- type: -{type(html)}")
-    print(f"\t- content: {(html)}")
-    print(f'\t- utf8 content: {html.decode("utf-8", "replace")}')
+"""Fetches data from given URL"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+            as response:
+        html = response.read()
+
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
