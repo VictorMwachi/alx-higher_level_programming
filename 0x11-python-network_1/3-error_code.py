268bb60a5a315d1bb991489333679913d3ddbb4e
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-01-09 21:40:21 +0300

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
         with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
     except error.HTTPError as error:
-                  print("Error code", error.code)
+                  print("Error code:", error.code)
