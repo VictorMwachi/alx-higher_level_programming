0f14cbfcd65786a3a1f9bfe1fddc2530a4dc2cfe
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-01-09 12:20:13 +0300

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-# Lists all City objects from the database hbtn_0e_101_usa.
+"""
+Lists all City objects from the database hbtn_0e_101_usa.
+"""
+
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -15,4 +19,3 @@ if __name__ == "__main__":
 
     for city in session.query(City).order_by(City.id):
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
-        
