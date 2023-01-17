5cbfa7a01a88e6c6b2a6b54150ba9bfd430f70af
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-01-17 23:11:02 +0300

    5-hbtn_header.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL
-and displays the body of the response (decoded in utf-8).
+Python script that takes in a URL, sends a request to
+the URL and displays the body of the response (decoded in utf-8).
+You have to manage urllib.error.HTTPError exceptions
+and print: Error code: followed by the HTTP status code
 """
 
 
+import sys
+from urllib import request, error
 if __name__ == '__main__':
-    import urllib.request
-    import urllib.error as error
-    import sys
-
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            response.read().decode('utf-8')
+        with request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
     except error.HTTPError as error:
         print("Error code:", error.code)
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+"""Displays the X-Request-Id header variable of a request to a given URL
 """
-Python script that takes in a URL, sends a request to the URL and
-displays the value of the variable X-Request-Id in the response header
-"""
+import sys
+import requests
 
 
-if __name__ == '__main__':
-    import requests
-    import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-    r = requests.get(sys.argv[1])
-    print(r.headers['X-Request-Id'])
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
