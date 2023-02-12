071b5b552f454ddb24a1f7b05bc8224a6cdc96c6
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-02-12 16:35:33 +0300

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,6 +5,14 @@ request(url + process.argv[2], (error, response, body) => {
   if (error) {
     console.log(error);
   }
-  const json = JSON.parse(body);
-  console.log(json.title);
+  const json = JSON.parse(body).characters;
+  json.forEach(personUrl => {
+    request(personUrl, (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      const bioData = JSON.parse(body);
+      console.log(bioData.name);
+    });
+  });
 });
